bf243a3d7b230ef193b30b5ced9ef55b40a35ef0
Oscar Montes <oscmcojc1@live.com.mx>
2020-03-29 17:35:18 -0600

    Widget chart bar creado

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
 import './models/transaction.dart';
+import './widgets/chart.dart';
 
 void main() => runApp(MyApp());
 
@@ -47,6 +48,14 @@ class _MyHomePageState extends State<MyHomePage> {
 //        id: 't2', title: 'new caca', amount: 22.99, date: DateTime.now()),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((transaction) {
+      return transaction.date.isAfter(DateTime.now().subtract(
+        Duration(days: 7),
+      ));
+    }).toList();
+  }
+
   void _addNewTransaction(String titleOfTx, double amountOfTx) {
     final newTransaction = Transaction(
       title: titleOfTx,
@@ -85,14 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text('CHART'),
-              elevation: 5,
-            ),
-          ),
+          Chart(_recentTransactions),
           TransactionList(_userTransactions)
         ],
       ),
